0d4170214be2b63eb2bb4108e3c0bc0cf7eb9828
William <btrigona@gmail.com>
2020-06-04 10:01:25 -0400

    Inital Commit

---
+++ src/test/java/co/grandcircus/movie/APICapstone/ApiCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package co.grandcircus.movie.APICapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
